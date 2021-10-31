8deadb3ef90a69af35e3177d412f99a2cc994bbc
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-31 09:15:39 +0300

    Define sources route

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_source
 
 # Views
 @app.route('/')
@@ -19,3 +20,14 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
+
+@app.route('/sources')
+def sources():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+  
+    source=get_source('sources')
+    title = " HOME-Welcome to the best News online website"
+    return render_template('source.html',title=title,sources=source)
