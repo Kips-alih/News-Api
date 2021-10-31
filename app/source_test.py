c4aa7cef55f9c2855fc28e7f02be5e71e7a58138
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-31 22:43:58 +0300

    Add Article class

--- app/source_test.py
+++ app/source_test.py
@@ -1,6 +1,8 @@
 import unittest
 from models import source
 Source=source.Source
+Article=source.Article
+
 
 
 class SourceTest(unittest.TestCase):
@@ -17,6 +19,22 @@ class SourceTest(unittest.TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
 
+
+
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Article('Kip','Python Must Be Crazy','A thrilling new Python Series','https://readwrite.com/2021/10/18/how-to-avoid-keyword-cannibalization/','https://images.readwrite.com/wp-content/uploads/2021/10/keyword-cannibalization.jpg','2021-10-29T15:31:00Z')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Article))
 
 if __name__ == '__main__':
     unittest.main()
