72900738e03511e0add6f30323b1a0c6114e1b30
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-10-31 18:33:20 +0300

    Add a get_news function and modify the routes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
-from .request import get_source
+from .request import get_source,get_news
+
 
 # Views
 @app.route('/')
@@ -9,25 +10,20 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    source=get_source('sources')
 
     title = 'Home - Welcome to The Best News Articles Preview Website'
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title,sources=source)
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html',id = movie_id)
-
-@app.route('/sources')
-def sources():
+@app.route('/articles/<source>',methods=['GET']) 
+def articles(source):
 
     '''
     View root page function that returns the index page and its data
+
     '''
-  
-    source=get_source('sources')
-    title = " HOME-Welcome to the best News online website"
-    return render_template('source.html',title=title,sources=source)
+    articles=get_news(source)
+    title='Articles - Welcome to The Best News Articles Preview Website' 
+
+    return render_template('articles.html',title=title,articles=articles,source=source)
+
